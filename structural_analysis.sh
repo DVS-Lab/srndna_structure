d9dcaedec20e6a66f1e10b582476c4a233704b4e
Caleb Haynes <calebhaynes0@gmail.com>
2019-11-14 09:43:33 -0500

    Update structural_analysis.sh

--- structural_analysis.sh
+++ structural_analysis.sh
@@ -4,4 +4,25 @@
 MAINDATADIR=/data/projects/srndna-structure/Data
 MAINOUTPUTDIR=/data/projects/srndna-structure/analyses_output 
 
-fsl_anat -d MAINDATADIR -o MAINOUTPUTDIR
+#make sub list 
+anats = /srndna-structure/Data/anats/*
+
+#change anats_test to anats when tested & changed in file system
+FIRSTDIR = /data/projects/srndna-structure/Data/anats_test/FIRST
+
+FASTDIR = /data/projects/srndna-structure/Data/anats_test/FAST
+
+#FIRST PROC
+cd FIRSTDIR
+for file in sub_list:
+    run_first_all -i {file}
+cd MAINDATADIR
+
+#FAST PROC
+cd FASTDIR
+for file in sub_list:
+    run_first_all -i {file}
+cd MAINDATADIR
+
+
+#VBM PROC
